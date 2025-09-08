44f8d6342fe19f54dfd5915f1de70a23fd85f10a
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-09-08 22:45:34 +0300

    Added register and product-detail page

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import main_page,catalog
+from .views import main_page,catalog,product_detail
 
 
 app_name = 'main'
@@ -8,4 +8,5 @@ app_name = 'main'
 urlpatterns = [
     path('',main_page, name='main_page'),
     path('catalog',catalog, name='catalog'),
+    path('product-detail', product_detail, name='product_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from main.models import Category, Size
+
 
 def main_page(request):
     return render(request,'main/main.html')
 
 def catalog(request):
-    return render(request, 'main/catalog.html')
+    categories = Category.objects.all()
+    sizes = Size.objects.all()
+    return render(request, 'main/catalog.html',{'categories':categories, 'sizes':sizes})
+
+def product_detail(request):
+    return render(request, 'main/product_detail.html')
