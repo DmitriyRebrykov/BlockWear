e6b81df2e59aaf412f34d9e6ba37e84ad724401a
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-09-10 12:48:31 +0300

    Added the main and cart applications along with templates

--- main/models.py
+++ main/models.py
@@ -30,12 +30,10 @@ class Product(models.Model):
     slug = models.SlugField(max_length=58, unique=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     color = models.CharField(max_length=100)
     main_image = models.ImageField(upload_to='products/main/')
-
-    status_discount = models.BooleanField(default=False)
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name = 'products')
 
