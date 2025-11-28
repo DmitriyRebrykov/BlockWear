485d273bb7dae39f7c7ad706535933e1ca3da786
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-11-28 13:58:53 +0200

    Added users models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,50 @@
+from turtle import mode
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.html import strip_tags
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password= None, **extra_fields):
+        if not email:
+            raise ValueError('The email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff = True')
+            
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_staff = True')
+            
+        return self.create_user( email, first_name, last_name, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length = 254)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    company = models.CharField(max_length = 100, blank=True, null=True)
+    address1 = models.CharField(max_length = 100, blank=True, null=True)
+    address2 = models.CharField(max_length = 100, blank=True, null=True)
+    city = models.CharField(max_length = 100, blank=True, null=True)
+    country = models.CharField(max_length = 100, blank=True, null=True)
+    province = models.CharField(max_length = 100, blank=True, null=True)
+    postal_code = models.CharField(max_length = 100, blank=True, null=True)
+    phone = models.CharField(max_length = 100, blank=True, null=True)
+    
+    username = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS= ['first_name', 'last_name']
+    
+    def clean(self):
+        for field in ['company','address1','address2','city','country','province','postal_code','phone']:
+            value = getaatr(self, field)
+            if value:
+                setattr(self, field, strip_tags(value))
+        
